61586f1e0efe14af51f6168cb30a3c3f1fee352e
Shawn Swineford <psswineford@gmail.com>
2023-02-09 10:40:45 -0800

    fixed more styling

--- PantryApplication-FE/src/app/components/add-recipe/add-recipe.component.ts
+++ PantryApplication-FE/src/app/components/add-recipe/add-recipe.component.ts
@@ -21,7 +21,57 @@ export class AddRecipeComponent {
   ingredients5: string = ''
   userId: number = 0
 
+  showIngredients: boolean = true
+  showIngredients2: boolean = false
+  showIngredients3: boolean = false
+  showIngredients4: boolean = false
+  showIngredients5: boolean = false
+
+
   constructor(public uiservice: UiService, public pantryService: PantryService, public recipeService: RecipeService) { }
 
+  setShowIngredients2(): void {
+    console.log('this ran')
+    this.showIngredients = true
+    this.showIngredients2 = true
+    this.showIngredients3 = false
+    this.showIngredients4 = false
+    this.showIngredients5 = false
+  }
+  setShowIngredients3(): void {
+    this.showIngredients = true
+    this.showIngredients2 = true
+    this.showIngredients3 = true
+    this.showIngredients4 = false
+    this.showIngredients5 = false
+  }
+  setShowIngredients4(): void {
+    this.showIngredients = true
+    this.showIngredients2 = true
+    this.showIngredients3 = true
+    this.showIngredients4 = true
+    this.showIngredients5 = false
+  }
+  setShowIngredients5(): void {
+    this.showIngredients = true
+    this.showIngredients2 = true
+    this.showIngredients3 = true
+    this.showIngredients4 = true
+    this.showIngredients5 = true
+  }
+
+  returnShowIngredients2(): boolean {
+    return this.showIngredients2
+  }
+
+  returnShowIngredients3(): boolean {
+    return this.showIngredients3
+  }
+  returnShowIngredients4(): boolean {
+    return this.showIngredients4
+  }
+  returnShowIngredients5(): boolean {
+    return this.showIngredients5
+  }
 
 }
